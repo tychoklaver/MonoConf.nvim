14d005a054b620c3f5ffeb4882985a9fae9cc23e
Tycho <tycho053klaver@gmail.com>
2025-09-23 09:28:54 +0200

    New Changes

--- lua/mono/core/keymaps.lua
+++ lua/mono/core/keymaps.lua
@@ -6,6 +6,10 @@ keymap.set("n", "<leader>sh", "<C-w>s", {desc = "split window horizontally"})
 keymap.set("n", "<leader>se", "<C-w>=", {desc = "make split equal size"})
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", {desc = "close current split"})
 
+-- Increment/Decrement Int Values
+keymap.set("n", "<leader>+", "<C-a>", {desc = "increment number"})
+keymap.set("n", "<leader>-", "<C-x>", {desc = "decrement number"})
+
 --Tmux keybinds
 keymap.set("n", "<c-k>", ":wincmd k<CR>")
 keymap.set("n", "<c-j>", ":wincmd j<CR>")
@@ -19,3 +23,11 @@ keymap.set("n", "C-l", ":wincmd l<CR>")
 
 keymap.set("n", "<leader>ee", "<cmd>Oil --float<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
 keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+
+-- move selected text in visual mode
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Add whitespace to selected text in visual mode
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
